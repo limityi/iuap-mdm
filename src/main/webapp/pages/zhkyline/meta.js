70de93ced87dc30f4f2c31c7d7242c89ce71e646
kafun18 <513112374@qq.com>
2019-01-04 16:03:33 +0800

    meta

---
+++ src/main/webapp/pages/zhkyline/meta.js
@@ -0,0 +1,35 @@
+var metaCardTable = {
+		 meta: {
+                    id:{
+                    
+                    },   
+                    code:{
+                    
+                    },
+                    lcode:{
+                    	
+                    },
+                    name:{
+                    
+                    },   
+                    mcode:{
+                    
+                    },   
+                    mdm_pk:{
+                    
+                    },
+                    mdm_code:{
+                    	
+                    },
+                    name1:{
+                    	
+                    },
+                    line_level:{
+                    	
+                    },
+					lline_level:{
+                    	
+                    } 
+                       
+	            }
+	}
